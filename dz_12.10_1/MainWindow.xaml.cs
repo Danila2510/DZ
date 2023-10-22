5658aafa14dc10d4467e5425148e43b958368db5
Danila <Danila@DESKTOP-B319KEU>
2023-10-22 13:33:20 +0300

    Добавьте файлы проекта.

---
+++ dz_12.10_1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dz_12_10_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        private string lang = "Eng";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void b_change_lng_Click(object sender, RoutedEventArgs e)
+        {
+            if (lang == "Eng")
+            {
+                ResourceDictionary newDictionary = new ResourceDictionary();
+                newDictionary.Source = new Uri("Resources/lang.ua-UA.xaml", UriKind.Relative);
+                Application.Current.Resources.MergedDictionaries[0] = newDictionary;
+                lang = "Ukr";
+            }
+            else
+            {
+                ResourceDictionary newDictionary = new ResourceDictionary();
+                newDictionary.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
+                Application.Current.Resources.MergedDictionaries[0] = newDictionary;
+                lang = "Eng";
+            }
+        }
+    }
+}
---
+++ dz_12.10_2/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using My_Localization_1_1.Resourses;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dz_12_10_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool lang_change = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+
+        private void UpdateUI()
+        {
+            Title = lang.winTitle;
+            l_name.Content = lang.l_name;
+            l_surname.Content = lang.l_surname;
+            l_phone.Content = lang.l_phone;
+            b_cancel.Content = lang.b_cancel;
+            b_change_lng.Content = lang.b_change_lng;
+        }
+
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void Change_lng_Click(object sender, RoutedEventArgs e)
+        {
+            if (lang_change)
+            {
+                MenuLangEN_Click(sender, e);
+                lang_change = false;
+            }
+            else
+            {
+                MenuLangUA_Click(sender, e);
+                lang_change = true;
+            }
+        }
+
+        private void MenuLangUA_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("uk");
+            UpdateUI();
+        }
+
+        private void MenuLangEN_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+            UpdateUI();
+        }
+    }
+}
